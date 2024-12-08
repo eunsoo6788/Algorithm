1e468a0a89f55433780e6d0ea290a980cb07dce0
ensu <ensu6788@gmail.com>
2024-12-08 23:47:21 +0900

    2024년 12월 8일

--- src/backjoon/datastructure/Solution5107.java
+++ src/backjoon/datastructure/Solution5107.java
@@ -3,10 +3,7 @@ package backjoon.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution5107 {
 
@@ -17,90 +14,62 @@ public class Solution5107 {
     public static void main(String[] args) throws IOException {
 
         /**
-         * 마니또 5107
+         * 아침 태권도 29197
          * 자료구조
          * 실버 1
          * sol
          */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testcase = 1;
-        while (true) {
 
+        int num = Integer.parseInt(br.readLine());
 
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                break;
-            }
+        int[][] arr = new int[num][2];
+        for (int i = 0 ; i < num ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            str = new String[num][2];
-
-            for (int i = 0 ; i < num ; i++) {
-
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-                String start = st.nextToken();
-                if (start.equals("0")){
-                    break;
-                }
-                String end = st.nextToken();
-
-                str[i][0] = start;
-                str[i][1] = end;
-
-
-            }
-
-
-            visited = new boolean[num];
-
-
-            int count = 0;
-
-            for (int i = 0 ; i < num ; i++) {
-
-                if (!visited[i]) {
-                    bfs(i);
-                    count++;
-                }
-            }
-
-
-            System.out.println(testcase + " " + count);
-
-            testcase++;
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
 
+        Arrays.sort(arr, (o1,o2) -> {
 
+            double distance1 = Math.sqrt(o1[0]*o1[0] + o1[1] * o1[1]);
+            double distance2 = Math.sqrt(o2[0]*o2[0] + o2[1] * o2[1]);
 
-    }
+            return Double.compare(distance1,distance2);
+        });
 
-    private static void bfs(int index) {
+        int count= 0;
+        boolean[] visited = new boolean[num];
+        for (int i = 0 ; i < num ; i++) {
 
-        Queue<String[]> queue = new LinkedList<>();
+            if (!visited[i]) {
 
-        queue.add(new String[]{str[index][0], str[index][1]});
-        visited[index] = true;
+                visited[i] = true;
+                count++;
 
+                for (int j = 0 ; j < num ; j++) {
+                    if (!visited[j]) {
 
-        while (!queue.isEmpty()) {
+                        int a1 = arr[j][0] / arr[i][0];
+                        int a2 = arr[j][1] / arr[i][1];
 
-            String[] poll = queue.poll();
+                        if (a1 == a2) {
+                            visited[j] = true;
+                        }
 
-
-            for (int i = 0 ; i < str.length ; i++) {
-                if (!visited[i]) {
-                    if (poll[1].equals(str[i][0])) {
-                        visited[i] = true;
-                        queue.add(new String[]{str[i][0], str[i][1]});
                     }
-
                 }
+
+
             }
 
-
         }
+
+
+        System.out.println(count);
 
 
     }
