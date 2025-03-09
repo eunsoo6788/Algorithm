352fb5ae5a5fb9129699c00e16eed52cdef096af
eunsoo6788 <155058284+eunsoo6788@users.noreply.github.com>
2025-03-09 23:37:11 +0900

    2025/03/09

--- src/backjoon/datastructure/Solution23843Re.java
+++ src/backjoon/datastructure/Solution23843Re.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution23843Re {
@@ -24,36 +23,51 @@ public class Solution23843Re {
         int size = Integer.parseInt(st.nextToken());
 
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {return o2-o1;});
+        int[] arr = new int[num];
         StringTokenizer stArr = new StringTokenizer(br.readLine(), " ");
         for (int i = 0 ; i < num ; i++) {
-            pq.add(Integer.parseInt(stArr.nextToken()));
+            arr[i] = Integer.parseInt(stArr.nextToken());
         }
 
+        Arrays.sort(arr);
+        int index = arr.length -1;
+        int time = 0;
 
-        int count = 0;
-        while (!pq.isEmpty()) {
-            count++;
-            int[] arr = new int[size];
-            for (int i = 0 ; i < size ; i++) {
-                if (!pq.isEmpty()) {
-                    arr[i] = pq.poll();
+        int[] queue = new int[size];
+
+        while(true) {
+
+            if (index >= 0){
+                for (int i = 0 ; i < size ; i++) {
+                    if (index >= 0 && queue[i] == 0) {
+                        queue[i] = arr[index];
+                        index--;
+                    }
                 }
             }
 
+            boolean flag = false;
             for (int i = 0 ; i < size ; i++) {
-                arr[i]--;
-                if (arr[i] > 0) {
-                    pq.add(arr[i]);
+                if (queue[i] > 0){
+                    flag = true;
+                    break;
                 }
             }
 
+            if (!flag){
+                break;
+            }
+
+            for (int i = 0 ; i < size ; i++) {
+                queue[i] -= 1;
+            }
+
+            time++;
+
         }
 
-        System.out.println(count);
 
-
-
+        System.out.println(time);
 
 
 
