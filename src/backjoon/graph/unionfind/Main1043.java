b4f12aef1a5eaf4fdf8934cfec26fa921aad0a19
eunsoojin <ensu6788@gmail.com>
2025-03-23 16:33:31 +0900

    2025년 03월 23일

--- src/backjoon/graph/unionfind/Main1043.java
+++ src/backjoon/graph/unionfind/Main1043.java
@@ -4,64 +4,69 @@ package backjoon.graph.unionfind;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main1043 {
+
+    /**
+     * 거짓말
+     * 골드4
+     * 유니온 파인드
+     * not sol
+     */
 
     private static int[] parent;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int num = Integer.parseInt(br.readLine());
-        int cityTripNum = Integer.parseInt(br.readLine());
+        int nodeNum = Integer.parseInt(st.nextToken());
+        int partyNum = Integer.parseInt(st.nextToken());
 
-
-        parent = new int [num+1];
-        for (int i = 1; i <= num; i++) {
+        parent = new int [nodeNum+1];
+        for (int i = 1; i <= nodeNum; i++) {
             parent[i] = i;
         }
 
-        for (int i = 0; i < num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < num; j++) {
-                int value = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int smartNodeNum = Integer.parseInt(st.nextToken());
+        int[] smartNodes = new int[nodeNum+1];
+        for (int i = 0; i < smartNodeNum; i++) {
+            smartNodes[Integer.parseInt(st.nextToken())] = 1;
+        }
 
-                if (value == 1) {
-                    union(i+1, j+1);
+
+        for (int i = 0 ; i < partyNum ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int personNum = Integer.parseInt(st.nextToken());
+
+            boolean found = false;
+            for (int j = 0 ; j < personNum ; j++) {
+                if (smartNodes[j] == 1) {
+                    found = true;
+                    break;
                 }
-
             }
-        }
 
-        int[] tripCity = new int[cityTripNum];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < cityTripNum; i++) {
-            tripCity[i] = Integer.parseInt(st.nextToken());
-        }
+            if (found) {
+                for (int j = 0 ; j < personNum ; j++) {
 
-
-        boolean tripSuccess = true;
-
-        for (int i = 0; i < cityTripNum-1; i++) {
-
-            int city1 = find(tripCity[i]);
-            int city2 = find(tripCity[i+1]);
-
-            if (city1 != city2) {
-                tripSuccess = false;
+                }
             }
 
         }
 
-        if (tripSuccess) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+
+
+
+
 
     }
+
 
     private static int find(int x) {
         if (x == parent[x]) {
@@ -81,11 +86,7 @@ public class Main1043 {
             } else {
                 parent[x] = y;
             }
-
         }
-
-
     }
-
 
 }
