635f3c717e69db7ec912810a2ac55fc0b89c5210
eunsoojin <ensu6788@gmail.com>
2025-04-14 21:19:56 +0900

    2025년 04월 19일

---
+++ src/backjoon/tmp/Main16236Re.java
@@ -0,0 +1,28 @@
+package backjoon.tmp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main16236Re {
+
+    /**
+     * 아기상어
+     * bfs
+     * 골드3
+     */
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+
+    private static void bfs(int x, int y) {
+
+
+    }
+
+}
